f1ca2ba55f839f671bda226ea5132fbc24517855
Henry Watkins <h.watkins@ucl.ac.uk>
2021-03-15 15:11:20 +0000

    black run

--- scribe.py
+++ scribe.py
@@ -3,42 +3,38 @@ import os
 
 import utils
 
+
 @click.group(help="A tool for helping scientific writing projects")
-@click.help_option('-h', '--help')
-@click.version_option('1.0', '-v', '--version', message='%(prog)s v%(version)s')
+@click.help_option("-h", "--help")
+@click.version_option("1.0", "-v", "--version", message="%(prog)s v%(version)s")
 def main():
     pass
 
+
 @main.command(help="create a new writing project skeleton file")
-@click.help_option('-h', '--help')
-@click.argument('filename')
+@click.help_option("-h", "--help")
+@click.argument("filename")
 def create(filename):
-    assert filename[-3:]==".md"
+    assert filename[-3:] == ".md"
     if os.path.isfile(filename):
-        raise ValueError('File with that name already exists')
+        raise ValueError("File with that name already exists")
     else:
+        print(utils.PREAMBLE)
         with open(filename, "w") as file:
             file.write("# Project skeleton\n")
             for section, questions in utils.MARKDOWN_TEMPLATE.items():
                 print("\nSection: {}".format(section))
+                print("-------------------------------------------")
                 file.write("\n## {}\n".format(section))
                 for question in questions:
-                    answer = input(question)
-                    new_lines = [answer]
-                    while True:
-                        new_line = input()
-                        if new_line:
-                            new_lines.append(new_line)
-                        else:
-                            break
-                    answer = "\n".join(new_lines)
+                    answer = utils.multiinput(question)
                     file.write(answer)
             file.close()
 
 
 @main.command(help="run the the writing process tests")
-@click.help_option('-h', '--help')
-@click.argument('filename')
+@click.help_option("-h", "--help")
+@click.argument("filename")
 def test(filename):
     file_dictionary = utils.read_project_markdown(filename)
     print("================= test session starts ===================")
@@ -48,10 +44,11 @@ def test(filename):
         for note in notes:
             print("\n{}".format(note))
             answer = input("Does the paragraph satisfy this note-test? y/[n]: ") or "n"
-            if answer=="n":
-                print('\033[31m'+"FAILED"+'\033[0m')
+            if answer == "n":
+                print("\033[31m" + "FAILED" + "\033[0m")
             else:
-                print('\033[32m'+"PASSED"+'\033[0m')
+                print("\033[32m" + "PASSED" + "\033[0m")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,17 +1,56 @@
+MARKDOWN_TEMPLATE = {
+    "Title ideas": ["What is a possible title of this work?: "],
+    "Narrative": ["How whould you describe the narrative of this work?: "],
+    "Key references": [
+        "What are the key references of this work? (e.g. Smith et al. 2017): "
+    ],
+    "Possible journals": ["What possible journals are there for this work?: "],
+    "Introduction": [
+        "What is the context for this work?: ",
+        "Why is it interesting?: ",
+    ],
+    "Previous work": [
+        "Where does this work follow from?: ",
+        "What does the field need?: ",
+    ],
+    "Methods": [
+        "What are you doing?: ",
+        "How does this improve on previous work?: ",
+        "What methods are being used?: ",
+    ],
+    "Experiments & results": [
+        "Describe the data you are using: ",
+        "What are your results?: ",
+        "What do your results mean in context?: ",
+    ],
+    "Conclusions": ["What are your conclusions?: "],
+    "Abstract": ["Write an abstract for your work: "],
+}
 
-MARKDOWN_TEMPLATE = {"Title ideas":["What is a possible title of this work?: "],
-"Narrative": ["How whould you describe the narrative of this work?: "],
-"Key references": ["What are the key references of this work? (e.g. Smith et al. 2017): "],
-"Possible journals": ["What possible journals are there for this work?: "],
-"Introduction": ["What is the context for this work?: ", "Why is it interesting?: "],
-"Previous work": ["Where does this work follow from?: ", "What does the field need?: "],
-"Methods": ["What are you doing?: ", "How does this improve on previous work?: ", "What methods are being used?: "] ,
-"Experiments & results": ["Describe the data you are using: ", "What are your results?: ", "What do your results mean in context?: "],
-"Conclusions": ["What are your conclusions?: "],
-"Abstract":["Write an abstract for your work: "]}
+PREAMBLE = """                SCRIBE
+    A small tool to help with scientific
+    writing projects. Answer the question
+    prompts with short notes that act as
+    unit tests. These notes should breifly
+    detail the purpose or operation of a
+    paragraph or short section. """
+
+
+def multiinput(question):
+    answer = input(question)
+    new_lines = [answer]
+    while True:
+        new_line = input()
+        if new_line:
+            new_lines.append(new_line)
+        else:
+            break
+    answer = "\n".join(new_lines)
+    return answer
+
 
 def read_project_markdown(filename):
-    assert filename[-3:]==".md"
+    assert filename[-3:] == ".md"
     with open(filename, "r") as file:
         lines = file.read().splitlines()
     lines_no_empty = [line for line in lines if line]
@@ -24,5 +63,8 @@ def read_project_markdown(filename):
         section_indices.append((section_index, last_index))
         last_index = section_index
     section_indices.reverse()
-    output_dict = {key:[lines_no_empty[k] for k in range(i+1, j)] for key, (i, j) in zip(MARKDOWN_TEMPLATE.keys(), section_indices)}
+    output_dict = {
+        key: [lines_no_empty[k] for k in range(i + 1, j)]
+        for key, (i, j) in zip(MARKDOWN_TEMPLATE.keys(), section_indices)
+    }
     return output_dict
