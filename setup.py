c3b12cc7b58e9d578b5a9943beb07b350489d664
Henry Watkins <h.watkins@hotmail.com>
2021-10-17 22:09:39 +0100

    pipenv conversion working

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='scribe',
+    version='0.0',
+    py_modules=['scribe'],
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scribe = scribe.scribe:cli',
+        ],
+    },
+)
